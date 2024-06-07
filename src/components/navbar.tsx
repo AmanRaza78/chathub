bf19dcbfaa5d332aeb641f2b93ddca34b6f9e85f
AmanRaza78 <razakhanaman9@gmail.com>
2024-06-07 18:11:48 +0530

    kinde auth setup complete

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,29 +1,43 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Navbar(){
-    return(
-        <nav className="border-b bg-background h-[10vh] flex items-center">
-            <div className="container flex justify-between items-center">
-                <Link href="/">
-                    <h1 className="text-3xl text-primary font-bold">ChatHub</h1>
-                </Link>
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+      <div className="container flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-3xl text-primary font-bold">ChatHub</h1>
+        </Link>
 
-                <div className="flex items-center gap-x-5">
-                    <ModeToggle/>
+        <div className="flex items-center gap-x-5">
+          <ModeToggle />
 
-                    <div className="flex items-center gap-x-5">
-                        <Link href="/">
-                            <Button>Login</Button>
-                        </Link>
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              {" "}
+              <Button>Logout</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Login</Button>
+              </LoginLink>
 
-                        <Link href="/">
-                            <Button>Sign Up</Button>
-                        </Link>
-                    </div>
-                </div>
+              <RegisterLink>
+                <Button>Sign Up</Button>
+              </RegisterLink>
             </div>
-        </nav>
-    )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
