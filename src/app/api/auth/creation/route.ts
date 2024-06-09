da4c7c422907c1d2010f79c1b7e0e616ccec1fad
AmanRaza78 <razakhanaman9@gmail.com>
2024-06-09 11:34:57 +0530

    fixed the redirect url for production

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -3,32 +3,35 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 import { unstable_noStore as noStore } from "next/cache";
 
-export async function GET(){
-    noStore()
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
+export async function GET() {
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if(!user || user===null || !user.id){
-        throw new Error("Something went wrong")
-    }
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong");
+  }
 
-    const dbuser = await prisma.user.findUnique({
-        where:{
-            id:user.id
-        }
-    })
+  const dbuser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
 
-    if(!dbuser){
-        await prisma.user.create({
-            data:{
-                id:user.id,
-                firstname: user.given_name ?? "",
-                lastname: user.family_name ?? "",
-                email:user.email ?? ""
+  if (!dbuser) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        firstname: user.given_name ?? "",
+        lastname: user.family_name ?? "",
+        email: user.email ?? "",
+      },
+    });
+  }
 
-            }
-        })
-    }
-
-    return NextResponse.redirect("http://localhost:3000/chat")
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/chat"
+      : "https://chathub-theta.vercel.app/chat"
+  );
 }
